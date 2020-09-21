39d996b90ce9bafb2f88d2a73574805dc126a9d5
isaac-oliveira <isaacsantos9876@gmail.com>
2020-09-21 08:28:30 -0300

    Create Messages Screen

---
+++ mobile/src/pages/Messages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Messages = () => {
+	return <Container />;
+};
+
+export default Messages;
--- mobile/src/routes/AppRouter.tsx
+++ mobile/src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import DrawerRouter from './DrawerRouter';
+import Messages from '../pages/Messages';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -8,6 +9,7 @@ const AppRouter = () => {
 	return (
 		<Navigator screenOptions={{ headerShown: false }}>
 			<Screen name="Drawer" component={DrawerRouter} />
+			<Screen name="Messages" component={Messages} />
 		</Navigator>
 	);
 };
