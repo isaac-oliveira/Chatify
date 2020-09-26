fca853ecdc43a709b624ce851baf4492a47be8fa
isaac-oliveira <isaacsantos9876@gmail.com>
2020-09-25 21:57:22 -0300

    Get api users

--- mobile/src/pages/Users/index.tsx
+++ mobile/src/pages/Users/index.tsx
@@ -1,28 +1,35 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ListRenderItem, ListRenderItemInfo } from 'react-native';
 
 import HeaderSearch from '../../components/HeaderSearch';
 import List from '../../components/List';
 import UserItem, { User } from '../../components/UserItem';
+import api from '../../services/api';
 
 import { Container } from './styles';
-
-const data = [
-	{ id: 1, name: 'Isaac' },
-	{ id: 2, name: 'Iasmim' },
-] as User[];
 
 type ItemRender = ListRenderItem<User>;
 type ItemInfo = ListRenderItemInfo<User>;
 
 const Users = () => {
+	const [loading, setLoading] = useState<boolean>(true);
+	const [users, setUsers] = useState<User[]>([]);
 	const { navigate } = useNavigation();
+
+	useEffect(() => {
+		api.get('/users').then((response) => {
+			const data = response?.data as User[];
+			setUsers(data);
+			setLoading(false);
+		});
+	}, []);
 
 	const renderItem: ItemRender = (props: ItemInfo) => {
 		function handleItem() {
 			navigate('Messages', {
 				userId: props.item.id,
+				name: props.item.name,
 			});
 		}
 		return <UserItem user={props.item} onPress={handleItem} />;
@@ -32,7 +39,8 @@ const Users = () => {
 		<Container>
 			<HeaderSearch title="Usuários" onChangeSearch={() => {}} />
 			<List<User>
-				data={data}
+				loading={loading}
+				data={users}
 				keyExtractor={(item) => String(item.id)}
 				renderItem={renderItem}
 			/>
