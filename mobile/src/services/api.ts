d7a039421b69c13f9056a6afb11185d74191aa9a
isaac-oliveira <isaacsantos9876@gmail.com>
2020-09-25 21:55:35 -0300

    Add token to requests

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,8 +1,16 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import { create } from 'apisauce';
 
 const api = create({
 	baseURL: 'https://api-chatify.herokuapp.com',
 	timeout: 7000,
+});
+
+api.addAsyncRequestTransform(async (request) => {
+	const token = await AsyncStorage.getItem('@chatify/token');
+	if (token) {
+		request.headers.Authorization = `Bearer ${token}`;
+	}
 });
 
 export default api;
